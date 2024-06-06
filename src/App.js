d1dd74947dcfa02d324f7a568c4c5c7dc05c4623
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 04:20:32 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container, CssBaseline } from '@material-ui/core';
 import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <Router basename="/content-management-dashboard">
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-        </Switch>
-      </div>
+      <CssBaseline />
+      <Container>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+          </Switch>
+        </div>
+      </Container>
     </Router>
   );
 }
