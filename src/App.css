8c1dda90ba8b0469e66775f80278d2d39cacdc52
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 02:50:15 +0600

    Update App.css

--- src/App.css
+++ src/App.css
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import './App.css';  // Import CSS for styling
 
 function App() {
   return (
