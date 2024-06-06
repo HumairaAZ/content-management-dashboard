3c568410969b55113905e43a076e7446c005dc8a
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 05:38:50 +0600

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { useState, Suspense } from 'react';
 import './Dashboard.css';
 import { useQuery, gql } from '@apollo/client';
-import { TextField, Grid, Card, CardContent, Typography, CardMedia, Select, MenuItem, InputLabel, FormControl, Modal, Backdrop, Fade } from '@material-ui/core';
-import { Pagination, Skeleton, Alert } from '@material-ui/lab';
+import { TextField, Grid, Card, CardContent, Typography, CardMedia, Select, MenuItem, InputLabel, FormControl, CircularProgress, Skeleton } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import { motion } from 'framer-motion';
 
 const GET_POKEMONS = gql`
