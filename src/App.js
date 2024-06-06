6e9f65b10ed3f2cede3ef8632542786e1f2cd48a
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 04:11:36 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/content-management-dashboard">
       <div className="App">
         <Switch>
           <Route path="/" exact component={Dashboard} />
