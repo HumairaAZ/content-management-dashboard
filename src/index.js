051d5f8f9c16bc3d1fa3db6ad841fb360a4ef686
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 04:42:26 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
+import localforage from 'localforage';
 
 const client = new ApolloClient({
   uri: 'https://graphql-pokemon2.vercel.app/',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 
 ReactDOM.render(
   <ApolloProvider client={client}>
